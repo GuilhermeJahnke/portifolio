94fd576925e10244591e9762bf85295f9dafca3b
Guilherme Jahnke Vieira <guiguijv@gmail.com>
2022-02-27 16:40:48 -0300

    fix:  delay in displaying numbers

--- assets/js/topbar-virtual.js
+++ assets/js/topbar-virtual.js
@@ -236,7 +236,7 @@ $(document).ready(function() {
               {
                 number: num,
                 numberStep: comma_separator_number_step
-              }, 5000
+              }, 2000
             );
           });
           
